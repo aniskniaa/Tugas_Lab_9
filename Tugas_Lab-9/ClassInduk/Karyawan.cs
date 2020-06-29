8b452e9587b840fdda3d98e77a4823949e724db4
aniskniaa <rnia578@gmail.com>
2020-06-29 22:36:45 +0700

    inisialisasi project

---
+++ Tugas_Lab-9/ClassInduk/Karyawan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tugas_Lab_9.ClassInduk
+{
+    public abstract class Karyawan
+    {
+        public string Nik { get; set; }
+        public string Nama { get; set; }
+
+        public abstract double Gaji();
+
+        public abstract string jenis();
+    }
+}
